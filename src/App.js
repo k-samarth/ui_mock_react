9e1845a858a683dcb8a7180d48561861e82269da
Kamath <kkamath@galaxe.com>
2024-06-03 11:39:36 +0530

    Fixed multiple connection creation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import ChatWindow from './components/ChatWindow';
 import './Assets/CSS/styles.css'
+import socketIOClient from 'socket.io-client';
+
+const socket = socketIOClient('http://localhost:9090', {
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        "Authorization": "123Kamath123",
+        "userid": '123'
+      }
+    },
+  }
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ChatWindow/>
+        <ChatWindow socket={socket}/>
       </header>
     </div>
   );
--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import RequestManager from './RequestManager'
+import socketIOClient from 'socket.io-client';
 
-const ChatWindow = () => {
-
+const ChatWindow = (props) => {
     const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+    
+    useEffect(() => {
+
+        props.socket.on('message_get', (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [props.socket]);
+    
     function send_message(event) {
         event.preventDefault()
+        props.socket.emit('message_send',message)
         setMessage('')
     }
     function update_message(event) {
@@ -16,7 +26,12 @@ const ChatWindow = () => {
     <div>
         <label className='chat_input_label'>Message</label>
         <input type='text' className='chat_input' onChange={update_message} value={message}></input>
-        <RequestManager send_message = {send_message}/>
+        <button className='chat_send_button' onClick={send_message}>Send</button>
+        <ul>
+            {messages.map((data) => (
+                        <li key={data}>{data}</li>
+                    ))}
+        </ul>
     </div>
   )
 }
